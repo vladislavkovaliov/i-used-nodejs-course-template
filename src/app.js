78d5cea03ed8f919f6107e6f9f988d2b258cbd2e
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2020-10-14 20:27:31 +0300

    feat: adds logger for both uncaughtException and unhandledRejection, and morgan

--- src/app.js
+++ src/app.js
@@ -7,18 +7,20 @@ const morganBody = require('morgan-body');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const {
+  uncaughtExceptionLogger,
+  unhandledRejectionLogger
+} = require('./common/logger');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms')
 );
 morganBody(app);
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -26,9 +28,32 @@ app.use('/', (req, res, next) => {
   }
   next();
 });
-
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    statusCode: 500,
+    message: err.message
+  });
+});
+
+// setTimeout(() => {
+//   throw new Error('Oops!');
+// }, 2000);
+//
+// setTimeout(() => {
+//   Promise.reject(Error('Oops!'));
+// }, 4000);
+
+process.on('uncaughtException', err => {
+  uncaughtExceptionLogger.error(err.message);
+});
+
+process.on('unhandledRejection', err => {
+  unhandledRejectionLogger.error(err.message);
+});
 
 module.exports = app;
